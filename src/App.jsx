7b8ed6f3e00fc8917ca7bc85d9d404c22c13bb85
Muhammad Rafi Walidain <rafi.walidain81@gmail.com>
2026-02-26 14:42:44 +0700

    Fix import path for SpeedInsights component from Vercel

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter } from "react-router-dom";
 import { About, Contact, Experience, Hero, Navbar, Tech, Works, StarsCanvas } from "./components";
 import ComputersCanvas from "./components/canvas/Computers";
-import { SpeedInsights } from "@vercel/speed-insights/next";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 const App = () => {
   return (
